ebcbd2cea0bc1c4a1ebe09a300baba2525983f03
HossamUK <hossam.m.pro@gmail.com>
2021-05-27 02:56:44 +0300

    update

---
+++ Server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const SpotfiyApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ mess: "its working" });
+});
+
+app.post("/login", (req, res) => {
+  if (req.body.code) {
+    const spotfiyApi = new SpotfiyApi({
+      clientId: "4451beba5b2c42a4a9dbb72c109d2de5",
+      clientSecret: "7e8e8de8a20049878f7526e23fff4fab",
+      redirectUri: "http://localhost:3000/",
+    });
+    spotfiyApi
+      .authorizationCodeGrant(req.body.code)
+      .then((r) => {
+        res.json({
+          accessToken: r.body.access_token,
+          refreshToken: r.body.refresh_token,
+          expiresIn: r.body.expires_in,
+        });
+      })
+      .catch((err) => {
+        res.json({ mess: err });
+      });
+  } else {
+    res.json({ mess: "the code is missing" });
+  }
+});
+app.listen(5000, () => {
+  console.log("server is working");
+});
