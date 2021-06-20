0ec0a521577a06920a46cacf08f1a86a79657354
hossamUK <hossam.m.pro@gmail.com>
2021-06-20 07:38:49 -0700

    update

--- Client/src/components/mainPage/MainPage.tsx
+++ Client/src/components/mainPage/MainPage.tsx
@@ -17,6 +17,11 @@ export default function MainPage() {
         "1uNFoZAHBGtllmzznpCI3s",
         "53XhwfbYqKCa1cC15pYq2q",
         "5ZsFI1h6hIdQRw2ti0hz81",
+        "06HL4z0CvFAxyc27GXpf02",
+        "7vk5e3vY1uw9plTHJAMwjN",
+        "6M2wZ9GZgrQXHCFfjv46we",
+        "7dGJo4pcD2V6oG8kP0tJRR",
+        "1Xyo4u8uXC1ZmMpatF05PJ",
       ])
         .then((r: any) => {
           console.log(r);
