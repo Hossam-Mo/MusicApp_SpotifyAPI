395c2c1fb4d037def96ef42e509d95fd3ef3456d
hossamUK <hossam.m.pro@gmail.com>
2021-06-21 10:45:04 -0700

    update

--- Client/src/components/secPage/SecPage.tsx
+++ Client/src/components/secPage/SecPage.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import "./secPage.css";
+//   Spotfiy.getArtist()
 
+interface prames {
+  id: string;
+  type: string;
+}
 export default function SecPage() {
+  const Spotfiy = useSelector((state: any) => state.spotfiy);
+
+  const prames = useParams<prames>();
+
+  useEffect(() => {
+    console.log(prames);
+
+    if (prames.id && Spotfiy) {
+      Spotfiy.getArtist(prames.id)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [prames, Spotfiy]);
+
   return <div className="secPage">hello</div>;
 }
