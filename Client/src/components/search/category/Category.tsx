109661bfd14895f434f8b0f1aa456d56da2e3469
hossamUK <hossam.m.pro@gmail.com>
2021-07-04 18:11:34 -0700

    search

--- Client/src/components/search/category/Category.tsx
+++ Client/src/components/search/category/Category.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import categories from "../../../types/categories";
 import "./category.css";
 
 export default function Category({ id, name, icons }: categories) {
   return (
-    <div
+    <Link
+      to={`/category/${id}`}
       className="category"
       style={{ backgroundImage: `url(${icons[0].url})` }}
     >
       <h3>{name}</h3>
-    </div>
+    </Link>
   );
 }
