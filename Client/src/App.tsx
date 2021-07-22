c84b59bef747279c6c1d87c8b14266cc5adf0dfd
hossamUK <hossam.m.pro@gmail.com>
2021-07-21 18:56:10 -0700

    loding

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useEffect } from "react";
+import React from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { useDispatch } from "react-redux";
 import useAuth from "./hooks/useAuth";
 import { get_spotfiy, get_user } from "./redux/actionTypes";
@@ -9,13 +10,16 @@ import MainPage from "./components/mainPage/MainPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Leftslide from "./components/leftslide/Leftslide";
 import SeeAll from "./components/seeAll/SeeAll";
-import SecPage from "./components/secPage/SecPage";
+//import SecPage from "./components/secPage/SecPage";
 import Search from "./components/search/Search";
 import Mp3Player from "./components/Mp3Player/Mp3Player";
+
 const code = new URLSearchParams(window.location.search).get("code");
 const Spotfiy = new SpotifyWebApi({
   clientId: "4451beba5b2c42a4a9dbb72c109d2de5",
 });
+
+const SecPage = React.lazy(() => import("./components/secPage/SecPage"));
 
 function App() {
   const token = useAuth();
@@ -60,21 +64,24 @@ function App() {
       {code ? (
         <Router>
           <Leftslide></Leftslide>
-          <Switch>
-            <Route exact path="/seeAll/:pageName/:id?">
-              <SeeAll></SeeAll>
-            </Route>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/seeAll/:pageName/:id?">
+                <SeeAll></SeeAll>
+              </Route>
 
-            <Route exact path="/:type/:id">
-              <SecPage></SecPage>
-            </Route>
-            <Route exact path="/search">
-              <Search></Search>
-            </Route>
-            <Route exact path="/">
-              <MainPage></MainPage>
-            </Route>
-          </Switch>
+              <Route exact path="/:type/:id">
+                <SecPage></SecPage>
+              </Route>
+              <Route exact path="/search">
+                <Search></Search>
+              </Route>
+              <Route exact path="/">
+                <MainPage></MainPage>
+              </Route>
+            </Switch>
+          </Suspense>
+
           <Mp3Player></Mp3Player>
         </Router>
       ) : (
